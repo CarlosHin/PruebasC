8b6317874027f9181f138249e9d07ad6270db613
Carlos <carloshin98@gmail.com>
2018-09-13 20:02:38 +0200

    Primer

---
+++ multMatrices.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+  int filas1,filas2,col1,col2;
+  printf("Filas matriz 1:");
+  scanf("%d",&filas1);
+  printf("Columnas matriz 1: ");
+  scanf("%d",&col1);
+  printf("Filas matriz 2: ");
+  scanf("%d",&filas2);
+  printf("Columnas matriz 2: ");
+  scanf("%d",&col2);
+  printf("\nMatriz 1 %dx%d", filas1,col1);
+  return 0;
+}
